d441446c562b393193e81bbf6175f5eb14b1b7df
Newkey <847749909@qq.com>
2024-10-10 23:07:12 +0800

    修改

--- src/test/java/com/newkey/ai/OpenAiTest.java
+++ src/test/java/com/newkey/ai/OpenAiTest.java
@@ -1,8 +1,11 @@
 package com.newkey.ai;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.api.OpenAiApi;
@@ -12,16 +15,26 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest(classes = OpenAiTest.class)
 @RunWith(SpringRunner.class)
+@Slf4j
 public class OpenAiTest {
 
 
     @Test
     public void testOpenAi(){
 
-        OpenAiChatModel chatModel = new OpenAiChatModel(new OpenAiApi("https://xiaoai.plus", "sk-y8wsKGMEytYXYV4aC009D5Ca1c2b447a99E07d5951953d09"));
 
-        String s = "如何查看你是chatGpt的哪个版本？";
-        String call = chatModel.call(s);
-        System.out.println(call);
+        OpenAiApi openAiApi = new OpenAiApi("https://xiaoai.plus", "sk-y8wsKGMEytYXYV4aC009D5Ca1c2b447a99E07d5951953d09");
+        OpenAiChatModel chatModel = new OpenAiChatModel(openAiApi, OpenAiChatOptions.builder().withModel(OpenAiApi.ChatModel.GPT_3_5_TURBO_1106).build());
+
+        ChatClient chatClient = ChatClient.builder(chatModel).build();
+
+        String s = "鲁迅和周树人是什么关系？";
+
+        ChatClient.CallResponseSpec call = chatClient.prompt().advisors(new SimpleLoggerAdvisor(
+                request -> "Custom request: " + request.userText(),
+                response -> "Custom response: " + response.getResult()
+        )).user(s).call();
+
+        System.out.println(call.content());
     }
 }
